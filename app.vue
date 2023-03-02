03a8b02ee062774a2b3baf08e02065b886c30e72
Ayumi Takahashi <ayumee528@gmail.com>
2023-03-01 17:19:02 -0800

    Add google fonts

--- app.vue
+++ app.vue
@@ -1,3 +1,7 @@
 <template>
-  <div class="bg-red-100">{{ $t("welcome") }}</div>
+  <div class="bg-red-100">
+    <p class="font-sans">{{ $t("welcome") }}</p>
+    <p class="font-serif">{{ $t("welcome") }}</p>
+    <p class="font-jp">{{ $t("welcome") }}</p>
+  </div>
 </template>
---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        'sans': ['Nunito Sans', ...defaultTheme.fontFamily.sans],
+        'serif': ['Spectral', ...defaultTheme.fontFamily.serif],
+        'jp': ['Noto Sans JP', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+  ],
+}
