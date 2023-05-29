cb3a9ab0901d6855a81f3a2985fabaa439d98983
Ayumi Takahashi <ayumee528@gmail.com>
2023-05-29 08:44:00 -0700

    update the homepage japanese description

--- components/home/Hero.vue
+++ components/home/Hero.vue
@@ -3,7 +3,8 @@
     <div class="text-center z-20 relative">
       <h1 class="text-6xl lg:text-8xl font-semibold mb-4">Web3 for Billions</h1>
       <p class="text-xl lg:text-2xl">
-        {{ $t("meta.description") }}
+        Startale Labs is a company that makes web3 infrastructure and public
+        goods
       </p>
     </div>
   </div>
