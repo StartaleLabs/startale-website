f2301a5a1d555c585252950bbb186866f8571075
Ayumi Takahashi <ayumee528@gmail.com>
2023-06-28 01:47:51 -0700

    Add static blog page

--- components/blog/ArticleCard.vue
+++ components/blog/ArticleCard.vue
@@ -2,7 +2,11 @@
   <div class="border-b border-gray-300 py-8">
     <!-- post.slug is the slug of the article -->
     <a
-      :href="'/blog/' + post.slug"
+      :href="
+        locale === 'ja'
+          ? '/blog/38814'
+          : '/blog/startale-and-sony-network-communications-form-capital-38815'
+      "
       class="block sm:flex space-y-4 sm:space-y-0 sm:space-x-8 items-center overflow-hidden bg-space-gray group transition hover:no-underline"
     >
       <img
