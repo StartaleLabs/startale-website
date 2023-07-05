17f08b72cfe0a830d0dfc456ce1635caa9744bcb
Ayumi Takahashi <ayumee528@gmail.com>
2023-07-05 11:30:17 -0700

    Add a static page for redirect

---
+++ pages/blog/startale-and-sony-network-communications-form-capital-38815.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { watchEffect } from "vue";
+import { useRouter } from "vue-router";
+
+definePageMeta({
+  layout: false,
+});
+
+const router = useRouter();
+watchEffect(() => {
+  router.push("/blog/38815");
+});
+</script>
