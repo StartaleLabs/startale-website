d47e7cfbf6919210ebb75df8c3af5350b1d82ebd
Ayumi Takahashi <ayumee528@gmail.com>
2024-08-13 11:12:20 -0700

    add redirect to layout

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,38 @@
+<template>
+  <HomeHero />
+  <client-only>
+    <HomeParticleStars />
+    <HomeParticleLogo />
+  </client-only>
+  <div class="space-y-28 lg:space-y-40">
+    <HomeFeatures />
+    <HomeProducts />
+    <HomeLogos />
+    <!-- <HomeBlog /> -->
+    <HomeCallToAction />
+  </div>
+</template>
+
 <script setup lang="ts">
-navigateTo('https://startale.com', { redirectCode: 301 })
+import { meta } from "@/content/meta";
+const { t } = useI18n();
+const seoTitle = `${meta.siteName} - ${t("meta.tagline")}`;
+const seoDescription = t("meta.description");
+const seoUrl = meta.url;
+const seoImage = `${meta.image}common.png`;
+
+useServerSeoMeta({
+  title: () => seoTitle,
+  description: () => seoDescription,
+  ogTitle: () => seoTitle,
+  ogDescription: () => seoDescription,
+  ogImage: () => seoImage,
+  ogImageUrl: () => seoImage,
+  ogType: () => "website",
+  ogUrl: () => seoUrl,
+  twitterCard: () => "summary_large_image",
+  twitterTitle: () => seoTitle,
+  twitterDescription: () => seoDescription,
+  twitterImage: () => seoImage,
+});
 </script>
